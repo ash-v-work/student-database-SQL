8d1e2977637a919de454096d2303ca244fdc419e
ash-v-work <ashvwork22@gmail.com>
2026-05-09 12:52:54 +0530

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,34 @@
+CREATE DATABASE student_db;
+USE student_db;
 
+CREATE TABLE students (
+	Student_ID INT AUTO_INCREMENT PRIMARY KEY,
+	Name VARCHAR(100),
+	Email VARCHAR(100) UNIQUE,
+	Age INT,
+	City VARCHAR(100)
+	);
+
+CREATE TABLE courses (
+	Course_ID INT AUTO_INCREMENT PRIMARY KEY,
+	Course_Name VARCHAR(100),
+	Duration_Weeks INT
+	);
+
+CREATE TABLE enrollments (
+	Enrollment_ID INT AUTO_INCREMENT PRIMARY KEY,
+	Student_ID INT,
+	Course_ID INT,
+	Enrollment_Date DATE,
+	FOREIGN KEY(Student_ID) REFERENCES students(Student_ID),
+	FOREIGN KEY (Course_ID) REFERENCES courses(Course_ID)
+);
+
+CREATE TABLE marks (
+	Mark_ID INT AUTO_INCREMENT PRIMARY KEY,
+	Student_ID INT,
+	Course_ID INT,
+	Score INT,
+	FOREIGN KEY (Student_ID) REFERENCES students(Student_ID),
+	FOREIGN kEY (Course_ID) REFERENCES courses(Course_ID)
+);
